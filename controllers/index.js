231c322b28e36a9f3c89b010ef623dc0a705941f
salwazahramunir <salwazahramunir@gmail.com>
2024-10-08 13:45:14 +0700

    login done

---
+++ controllers/index.js
@@ -0,0 +1,17 @@
+class Controller {
+    static async login(request, response, next) {
+        try {
+            // logic login
+
+            response.status(200).json({
+                message: "success login"
+            })
+        } catch (error) {
+            response.status(500).json({
+                message: "internal server error"
+            })
+        }
+    }
+}
+
+module.exports = Controller
